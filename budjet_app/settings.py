9a98ec902a359cd202f97664eeba94ff7bae8010
jithincied <jithin.vv@cied.in>
2022-11-16 10:37:13 +0530

    update settings

--- budjet_app/settings.py
+++ budjet_app/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #libraries
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
+    
+    #v1
     'accounts',
     'app',
     'dashboard',
